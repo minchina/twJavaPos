9cd60afd60dd1ab46a105244d82536d347e41ad3
minchina <minchina91@gmail.com>
2014-12-17 13:23:43 +0800

    ncmao add:two promotion class

---
+++ src/main/java/com/tw/mnc/promotions/SecondHalf.java
@@ -0,0 +1,7 @@
+package com.tw.mnc.promotions;
+
+/**
+ * Created by minchina on 14-12-17.
+ */
+public class SecondHalf extends promition {
+}
---
+++ src/main/java/com/tw/mnc/promotions/promition.java
@@ -0,0 +1,8 @@
+package com.tw.mnc.promotions;
+
+/**
+ * Created by minchina on 14-12-17.
+ */
+public class promition {
+    private float promotionPrice;
+}
