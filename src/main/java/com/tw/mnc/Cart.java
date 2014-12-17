db30ec110e0aeca46d038ba37419ea42d283bcad
minchina <minchina91@gmail.com>
2014-12-17 13:12:40 +0800

    ncmao update:refactor add

--- src/main/java/com/tw/mnc/Cart.java
+++ src/main/java/com/tw/mnc/Cart.java
@@ -7,22 +7,51 @@ import java.util.List;
  * Created by minchina on 14-12-17.
  */
 public class Cart {
+
     private List<GoodItem> goodItemList = new ArrayList<>();
     private float totalPrice;
-    public void add(GoodItem goodItem){
-        goodItemList.add(goodItem);
-    }
 
-    public int numOf(GoodItem goodItem){
-        return goodItem.getNum();
+    public void add(int num,Good good){
+        GoodItem goodItem = new GoodItem(num,good);
+        goodItemList.add(goodItem);
+        updateTotalPrice();
     }
 
     public float getTotalPrice(){
+        return this.totalPrice;
+    }
+
+    public void updateTotalPrice(){
         float totalPrice = 0;
         for (GoodItem goodItem : goodItemList) {
             totalPrice += goodItem.getItemPrice();
         }
         this.totalPrice = totalPrice;
-        return this.totalPrice;
+    }
+
+    public void printTicket(){
+        printTitle();
+        printSubtotal();
+        printPriceTitle();
+        printPrice();
+    }
+
+    public void printTitle(){
+        System.out.println("购物明细（数量 单价 小计）");
+
+    }
+
+    public void printSubtotal(){
+        for(GoodItem item:goodItemList){
+            System.out.println(item.good.getName()+"   "+ item.getNum()+"   "+item.good.getPrice()+"   "+item.getItemPrice());
+        }
+    }
+
+    public void printPriceTitle(){
+        System.out.println("总计金额（优惠前  优惠后  优惠差价）");
+    }
+
+    public void printPrice(){
+        System.out.println(totalPrice);
     }
 }
