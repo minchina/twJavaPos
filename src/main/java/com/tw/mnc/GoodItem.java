df7a451d4d00038044b680137941f4366d3c6063
minchina <minchina91@gmail.com>
2014-12-17 22:18:32 +0800

    ncmao update:getItemPromotionPrice method

--- src/main/java/com/tw/mnc/GoodItem.java
+++ src/main/java/com/tw/mnc/GoodItem.java
@@ -1,19 +1,29 @@
 package com.tw.mnc;
 
+import com.tw.mnc.promotions.SecondHalf;
+
 /**
  * Created by minchina on 14-12-17.
  */
 public class GoodItem{
     public int num;
     public Good good;
+    public double promotionPrice;
 
     public GoodItem(int num,Good good){
         this.good = good;
         this.num = num;
+        this.promotionPrice = 0;
     }
 
-    public float getItemPrice(){
+    public double getItemPrice(){
         return good.getPrice()*num;
+    }
+
+    public double getItemPromotionPrice(){
+        SecondHalf secondHalf = new SecondHalf(num);
+        this.promotionPrice = secondHalf.getPromotionPrice(good.getPrice());
+        return this.promotionPrice;
     }
 
     public int getNum(){
