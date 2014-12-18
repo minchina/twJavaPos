768c230bddd05203f52bd66379fda86185dfff76
minchina <minchina91@gmail.com>
2014-12-18 15:54:34 +0800

    ncmao - update:printticket

--- src/main/java/com/tw/mnc/Cart.java
+++ src/main/java/com/tw/mnc/Cart.java
@@ -48,24 +48,7 @@ public class Cart {
 
 //以下为打印bill
     public void printTicket(){
-        printSubtotal();
-        printPrice();
+        new PrintTicket(goodItemList,this).printInformation();
+
     }
-
-    public void printSubtotal(){
-        System.out.println("购物明细（数量 单价 小计）");
-        for(GoodItem item:goodItemList){
-            System.out.println(item.good.getName()+"   "+ item.getNum()+"   "+item.good.getPrice()+"   "+item.promotionPrice);
-        }
-    }
-
-
-    public void printPrice(){
-        System.out.println("总计金额（优惠前  优惠后  优惠差价）");
-        System.out.println((getTotalPromotionPrice())
-                +"   "+getTotalPrice()+"   "
-                +getTotalPromotionPrice()+"    "+
-                (getTotalPrice()-getTotalPromotionPrice()));
-    }
-
 }
