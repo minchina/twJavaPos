db3f02e310712ff86fa47c2fbfba8ab28595c9b6
minchina <minchina91@gmail.com>
2014-12-18 09:45:27 +0800

    ncmao update:promotionPrice compute

--- src/main/java/com/tw/mnc/promotions/Discount.java
+++ src/main/java/com/tw/mnc/promotions/Discount.java
@@ -4,4 +4,13 @@ package com.tw.mnc.promotions;
  * Created by minchina on 14-12-18.
  */
 public class Discount extends promition {
+    public double rate;
+
+    public Discount(Double rate){
+        this.rate = rate;
+    }
+
+    public Double getPromotionPrice(Double price){
+        return price * (rate / 100);
+    }
 }
