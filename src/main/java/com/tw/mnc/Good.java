ed3395c51695e00904db2069d9005f20f8356c81
minchina <minchina91@gmail.com>
2014-12-18 15:55:31 +0800

    ncmao - update:add List to save promotion

--- src/main/java/com/tw/mnc/Good.java
+++ src/main/java/com/tw/mnc/Good.java
@@ -1,5 +1,10 @@
 package com.tw.mnc;
 
+import com.tw.mnc.promotions.Promotion;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by minchina on 14-12-17.
  */
@@ -7,12 +12,18 @@ public class Good{
     public String barcode;
     private double price;
     private String name;
+    public List<Promotion> promotionList = new ArrayList<>();
 
 
     public Good(String barcode,double price){
+//        super("noType");
         this.barcode = barcode;
         this.price = price;
         this.name = barcode.substring(0,4)+barcode.substring(barcode.length()-1);
+    }
+
+    public void addPromotion(Promotion promotion){
+        promotionList.add(promotion);
     }
 
     public double getPrice() {
