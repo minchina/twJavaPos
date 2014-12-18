f072a48607825508f158a7302160803e04723606
minchina <minchina91@gmail.com>
2014-12-18 15:56:23 +0800

    ncmao - update:modify promotion method

--- src/main/java/com/tw/mnc/GoodItem.java
+++ src/main/java/com/tw/mnc/GoodItem.java
@@ -1,7 +1,5 @@
 package com.tw.mnc;
 
-import com.tw.mnc.promotions.Discount;
-import com.tw.mnc.promotions.SecondHalf;
 
 /**
  * Created by minchina on 14-12-17.
@@ -14,11 +12,7 @@ public class GoodItem{
     public GoodItem(int num,Good good){
         this.good = good;
         this.num = num;
-        this.promotionPrice = good.getPrice();
-    }
-
-    public boolean isPromotionFor(Good good,String promotionRule){
-        return true;
+        this.promotionPrice = good.getPrice() * num;
     }
 
     public double getItemPrice(){
@@ -30,14 +24,8 @@ public class GoodItem{
     }
 
     public void updateItemPromotionPrice(){
-        if (isPromotionFor(good,"second_half")){
-            SecondHalf secondHalf = new SecondHalf(num);
-            this.promotionPrice = secondHalf.getPromotionPrice(promotionPrice);
-        }
-
-        if (isPromotionFor(good,"rating")){
-            Discount discount = new Discount(80);
-            this.promotionPrice = discount.getPromotionPrice(promotionPrice);
+        for (int i=0;i<good.promotionList.size();i++){
+            this.promotionPrice = good.promotionList.get(i).getPromotionPrice(num,promotionPrice);
         }
     }
 
