b89442aea168fee8f8887ff27d5f9e290a8e7090
minchina <minchina91@gmail.com>
2014-12-18 14:01:46 +0800

    ncmao - rename

--- src/main/java/com/tw/mnc/promotions/Promotion.java
+++ src/main/java/com/tw/mnc/promotions/Promotion.java
@@ -3,6 +3,6 @@ package com.tw.mnc.promotions;
 /**
  * Created by minchina on 14-12-17.
  */
-public class promition {
-    private float promotionPrice;
+public class Promotion {
+    public double rate;
 }
