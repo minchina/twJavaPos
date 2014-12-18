b2b6cd2bb3e5c36c8ed7ba5c0a36278ef43b957e
minchina <minchina91@gmail.com>
2014-12-18 09:22:13 +0800

    ncmao update:promotionPrice is price after promotion

--- src/main/java/com/tw/mnc/promotions/SecondHalf.java
+++ src/main/java/com/tw/mnc/promotions/SecondHalf.java
@@ -1,7 +1,19 @@
 package com.tw.mnc.promotions;
 
+import sun.applet.Main;
+
 /**
  * Created by minchina on 14-12-17.
  */
 public class SecondHalf extends promition {
+    private int num;
+
+    public SecondHalf(int num){
+        this.num = num;
+    }
+
+    public double getPromotionPrice(double price){
+
+        return price*num-Math.floor(num / 2)*(price/2);
+    }
 }
