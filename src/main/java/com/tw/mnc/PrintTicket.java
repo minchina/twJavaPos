9c5e7b42002c4fa372580b341da745c1f5efb4aa
minchina <minchina91@gmail.com>
2014-12-19 11:27:40 +0800

    ncmao - update:out.println

--- src/main/java/com/tw/mnc/PrintTicket.java
+++ src/main/java/com/tw/mnc/PrintTicket.java
@@ -1,11 +1,14 @@
 package com.tw.mnc;
 
+import java.io.PrintStream;
 import java.util.List;
 
 /**
  * Created by minchina on 14-12-18.
  */
 public class PrintTicket {
+    public PrintStream out = System.out;
+
     Cart cart;
 
 
@@ -15,21 +18,21 @@ public class PrintTicket {
     }
 
     public void printInformation(){
-        printSubtotal();
-        printPrice();
+        printSubtotal(out);
+        printPrice(out);
     }
 
-    public void printSubtotal(){
-        System.out.println("购物明细（数量 单价 小计）");
+    public void printSubtotal(PrintStream out){
+        out.println("购物明细（数量 单价 小计）");
         for(GoodItem item:cart.goodItemList){
-            System.out.println(item.good.getName()+"   "+ item.getNum()+"   "+(int)item.good.getPrice()+"   "+(int)item.getItemPromotionPrice());
+            out.println(item.good.getName()+"   "+ item.getNum()+"   "+(int)item.good.getPrice()+"   "+(int)item.getItemPromotionPrice());
         }
     }
 
 
-    public void printPrice(){
-        System.out.println("总计金额（优惠前  优惠后  优惠差价）");
-        System.out.println(((int)cart.getTotalPromotionPrice())
+    public void printPrice(PrintStream out){
+        out.println("总计金额（优惠前  优惠后  优惠差价）");
+        out.println(((int)cart.getTotalPromotionPrice())
                 +"   "+(int)cart.getTotalPrice()+"   "
                 +(int)cart.getTotalPromotionPrice()+"    "+
                 (int)(cart.getTotalPrice()-cart.getTotalPromotionPrice()));
