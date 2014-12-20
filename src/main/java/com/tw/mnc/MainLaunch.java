7441c6dfe80bf0c22143d8e562764e3405ec27a5
minchina <minchina91@gmail.com>
2014-12-20 21:48:24 +0800

    ncmao - update:main launcher

--- src/main/java/com/tw/mnc/MainLaunch.java
+++ src/main/java/com/tw/mnc/MainLaunch.java
@@ -7,17 +7,13 @@ import com.tw.mnc.utility.Parser;
 
 public class MainLaunch {
     public static void main(String[] args){
-        Good apple = new Good("ITEM000001",40);
-        Good strawberry = new Good("ITEM000003",50);
-        Good pear = new Good("ITEM000005",60);
+
+        Good apple = new Good.Builder().barcode("ITEM000001").name("apple").price(40).promotion(new SecondHalf(), new Discount(75)).build();
+        Good strawberry = new Good.Builder().barcode("ITEM000001").name("strawberry").price(50).promotion(new SecondHalf()).build();
+        Good pear = new Good.Builder().barcode("ITEM000001").name("pear").price(60).promotion(new Discount(90)).build();
 
         Cart cart = new Cart();
 
-        apple.addPromotion(new SecondHalf());
-        strawberry.addPromotion(new SecondHalf());
-
-        apple.addPromotion(new Discount(75));
-        pear.addPromotion(new Discount(90));
 
 
         cart.add(5,apple);
